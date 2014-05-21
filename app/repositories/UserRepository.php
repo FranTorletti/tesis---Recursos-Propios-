cade0376dab69145b8b02cb83eee613d5d8291f2
Fran <frantorletti@gmail.com>
2014-05-21 16:12:54 -0300

    add UserRepository

---
+++ app/repositories/UserRepository.php
@@ -0,0 +1,29 @@
+<?php
+
+use Doctrine\ORM\EntityRepository;
+
+class UserRepository extends EntityRepository {
+
+    function isValidUser($email, $password) {
+        $criteria = array("email" => $email, "password" => $password);
+        $return = Model::getEM()->getRepository("User")->findOneBy($criteria);
+
+        return !empty($return);
+    }
+
+    function getByEmail($email) {
+        $criterio = array("email" => $email);
+        $return = Model::getEM()->getRepository("User")->findOneBy($criterio);
+
+        return ($return);
+    }
+
+    function getById($idUser) {
+        $criterio = array("id" => $idUser);
+        $return = Model::getEM()->getRepository("User")->findOneBy($criterio);
+
+        return ($return);
+    }
+}
+
+?>
